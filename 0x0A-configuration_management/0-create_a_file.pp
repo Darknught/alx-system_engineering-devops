b00d5bfe81024670291596bd96ab0fe7859760f2
Darknught <hassino66@yahoo.com>
2024-02-23 12:03:54 +0000

    code

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,10 @@
- # create file in /tmp
+# Create file in /tmp
 
 file { '/tmp/school' :
-  path => /tmp/school,
-  ensure => present,
-  mode => '0744',
-  owner => 'www-data',
-  group => 'www-data',
-  content => 'I love Puppet',
-}
+    path    => '/tmp/school',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
+    content => 'I love Puppet',
+    }
+

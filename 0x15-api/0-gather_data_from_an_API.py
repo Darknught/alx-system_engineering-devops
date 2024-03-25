8f4c4de148636b2625f0db3a1beda6ac44d66bdb
Darknught <hassino66@yahoo.com>
2024-03-25 16:03:21 +0000

    code

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -2,19 +2,6 @@
 """
 This script fetches and displays the TODO list progress of an
 employee from a REST API.
-It accepts an employee ID as input and prints the employee's name,
-the number of completed tasks,
-and the titles of the completed tasks. The script uses the requests
-library to make HTTP GET requests
-to the API and safely accesses dictionary values using the get method.
-
-Usage:
-    python script.py <employee_id>
-
-Requirements:
-    - The script must use the requests library to make HTTP requests.
-    - It must accept an employee ID as a command-line argument.
-    - The script should not be executed when imported as a module.
 """
 import requests
 import sys

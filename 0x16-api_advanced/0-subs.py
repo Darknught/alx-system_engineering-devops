237d12fdb551c7fc4afd725c132251bc532bc1a2
Darknught <hassino66@yahoo.com>
2024-04-09 12:10:01 +0000

    detailed comment

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,11 +6,10 @@ import requests
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {'User-Agent': 'Darknught@alx-system_engineering_devops'}
+    headers = {"User-Agent": "Darknught"}
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 404:
         return 0
-
-    Data = response.json().get('data')
-    return Data.get('subscribers')
+    Data = response.json().get("data")
+    return Data.get("subscribers")

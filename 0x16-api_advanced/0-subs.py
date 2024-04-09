3b0511558e6d0b7973447116aa37b0dd66534e1c
Darknught <hassino66@yahoo.com>
2024-04-09 11:33:31 +0000

    code

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,7 +6,7 @@ import requests
 def number_of_subscribers(subreddit):
     """The Function to query the API"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {'User-Agent': 'MyGuy'}  # a custom User-Agent to avoid errors
+    headers = {'User-Agent': 'MyGuy'}
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 404:

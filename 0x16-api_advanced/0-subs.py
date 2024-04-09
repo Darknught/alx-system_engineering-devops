67b3092b3b54718a3219a2419b6897a95260b522
Darknught <hassino66@yahoo.com>
2024-04-09 10:26:58 +0000

    Querying the Reddit API

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+A Function that queries the Reddit API and returns number of subscribers
+If an invalid subreddit is given, it should return 0
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """The Function to query the API"""
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'MyGuy'}  # a custom User-Agent to avoid errors
+
+    # Prevent following redirects
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
+        return 0

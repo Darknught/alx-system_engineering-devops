2fbfffaa7a5092007c754231d138397bd7e7d274
Darknught <hassino66@yahoo.com>
2024-04-12 15:08:26 +0000

    code

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,22 @@
+# Puppet manifest to automate debugging of Apache 500 Internal Server Error
+
+# Install strace package
+package { 'strace':
+  ensure => installed,
+}
+
+# Define exec resource to run strace
+exec { 'debug_apache':
+  command   => 'strace -p <apache_pid>',
+  path      => '/usr/bin:/bin',
+  logoutput => true,
+  require   => Package['strace'],
+}
+
+# Define exec resource to reproduce the issue with curl
+exec { 'curl_localhost':
+  command   => 'curl -sI 127.0.0.1',
+  path      => '/usr/bin:/bin',
+  logoutput => true,
+  require   => Exec['debug_apache'],
+}
